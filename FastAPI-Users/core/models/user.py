bc6cd088243196f1c754e16ccc2471ea12c81409
Vlad-Note <gricenkovlad007@gmail.com>
2025-02-02 22:51:33 +0300

    Create user

--- FastAPI-Users/core/models/user.py
+++ FastAPI-Users/core/models/user.py
@@ -1,16 +1,13 @@
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from collections.abc import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
 
-class User(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
-
-    __table_args__ = (
-        UniqueConstraint("foo", "bar"),
-    )
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    pass
